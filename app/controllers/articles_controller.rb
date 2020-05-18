40442fb557e5d5306006e8dc8a314b201a39d5ca
Jordan Handy <handy.jordan@gmail.com>
2020-05-18 12:55:54 -0400

    add error checking for article submissions, and flash to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,24 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new
+        # creat article instance variable for use with new submissions
+        @article = Article.new
     end
     def create
+        # create new article with instance variable
+        # values need to be whitelisted to be allowed
+        # to post
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # save the article
+        if @article.save
+          flash[:notice] = "Article was created successfully"
+        
+        # flash[:alert] = "Something went wrong"
+        # redirect to the page of the article
+        #redirect_to article_path(@article)
+          redirect_to @article
+        else
+          render 'new'
+        end
     end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create the home and about routes
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  # create only the "show" route
+  resources :articles, only: [:show, :index, :new, :create]
 end
