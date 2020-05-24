8878ef705ba026790957d95d0f5927a26e63c27d
Jordan Handy <handy.jordan@gmail.com>
2020-05-24 09:13:05 -0400

    add secure password field for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
     # - Make association between users and many articles 
     # - ensure data validation for the presence of a username, and character limits
     # - ensure email is in the correct format
+    # - ensure the users each have a secure password
     
     before_save {self.email = email.downcase}
     has_many :articles
     validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-
+    has_secure_password
 end
