1825481ab75d46efb9d6dcf80b3b49f5b170480e
Jordan Handy <handy.jordan@gmail.com>
2020-05-25 07:55:15 -0400

    add require user method to application controller, so user must be logged in as same user who wants to edit specific articles.  Use these methods as before_action to restrict activities in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 #ArticlesController inherits from ApplicationController
 class ArticlesController < ApplicationController
   before_action :set_article, only:[:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
         #Assign an item from the Article table to a variable
@@ -88,5 +90,11 @@ class ArticlesController < ApplicationController
     # returns the whitelisted params for use in the create and update actions
     def article_params_whitelist
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can only edit or delete your own article!"
+        redirect_to @article
     end
 end 
