15f955fbf1bedf35c732d2e55de07f6c82f06b44
Jordan Handy <handy.jordan@gmail.com>
2020-05-27 08:00:31 -0400

    add ability to delete user, destroy user session, and destroy all articles created by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     # - ensure the users each have a secure password
     
     before_save {self.email = email.downcase}
-    has_many :articles
+    has_many :articles, dependent :destroy
     validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
