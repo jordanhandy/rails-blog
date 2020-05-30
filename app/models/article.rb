49987fc73e8ec4ea648defb12375b736604a7dbf
Jordan Handy <handy.jordan@gmail.com>
2020-05-30 09:54:38 -0400

    add article category association model, to associate articles to categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ApplicationRecord
     # create association between Articles and a User
 
     belongs_to :user
+    has_many :article_categories
+    has_many :categories, through: :article_categories
     # ensures the title is present for any article to be saved
     # enforcing minimum data lengths for the title and the description
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
